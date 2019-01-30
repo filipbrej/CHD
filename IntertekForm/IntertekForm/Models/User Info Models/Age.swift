41d8a73ef6c35b719af2df5a1a0aae999b4de538
Filip <brej.filip@gmail.com>
2019-01-30 09:15:33 -0600

    Removed giant comment section
    Had these for reference.

--- IntertekForm/IntertekForm/Models/User Info Models/Age.swift
+++ IntertekForm/IntertekForm/Models/User Info Models/Age.swift
@@ -24,46 +24,4 @@ struct Age {
         }
         return ageArray
     }
-    
-    
-    //                Age(age: "1 month old"),
-    //                Age(age: "2 months old"),
-    //                Age(age: "3 months old"),
-    //                Age(age: "4 months old"),
-    //                Age(age: "5 months old"),
-    //                Age(age: "6 months old"),
-    //                Age(age: "7 months old"),
-    //                Age(age: "8 months old"),
-    //                Age(age: "9 months old"),
-    //                Age(age: "10 months old"),
-    //                Age(age: "11 months old"),
-    //                Age(age: "12 months old"),
-    //                Age(age: "13 months old"),
-    //                Age(age: "14 months old"),
-    //                Age(age: "15 months old"),
-    //                Age(age: "16 months old"),
-    //                Age(age: "17 months old"),
-    //                Age(age: "18 months old"),
-    //                Age(age: "19 months old"),
-    //                Age(age: "20 months old"),
-    //                Age(age: "21 months old"),
-    //                Age(age: "22 months old"),
-    //                Age(age: "23 months old"),
-    //                Age(age: "24 months old"),
-    //                Age(age: "3 years old"),
-    //                Age(age: "4 years old"),
-    //                Age(age: "5 years old"),
-    //                Age(age: "6 years old"),
-    //                Age(age: "7 years old"),
-    //                Age(age: "8 years old"),
-    //                Age(age: "9 years old"),
-    //                Age(age: "10 years old"),
-    //                Age(age: "11 years old"),
-    //                Age(age: "12 years old"),
-    //                Age(age: "13 years old"),
-    //                Age(age: "14 years old"),
-    //                Age(age: "15 years old"),
-    //                Age(age: "16 years old"),
-    //                Age(age: "17 years old")
-    
 }
